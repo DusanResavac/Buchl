56f49fe6de73c171d1ef797102cdf0dba679e476
Dusan Resavac <if20b207@technikum-wien.at>
2022-03-26 10:48:21 +0100

    finished homepage, books search page and started book detail page

--- src/main/java/at/technikumwien/buchl/DiscussionRepository.java
+++
@@ -1,9 +0,0 @@
-package at.technikumwien.buchl;
-
-import at.technikumwien.buchl.entity.Discussion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
-}
